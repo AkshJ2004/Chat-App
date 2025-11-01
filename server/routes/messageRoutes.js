bbc285bbbee991dd5604e82481718c73c378010d
AkshJ2004 <imperialaksh.2004@gmail.com>
2025-11-02 01:26:47 +0530

    Resolved Conflicts

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,5 +1,5 @@
 import express from  "express";
-import { getMessages, getUserForSidebar, markMessageAsSeen } from "../controllers/messageController";
+import { getMessages, getUserForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController";
 import {protectRoute} from "../middleware/auth.js";
 
 const messageRouter = express.Router();
@@ -7,6 +7,8 @@ const messageRouter = express.Router();
 messageRouter.get("/users",protectRoute,getUserForSidebar);
 messageRouter.get("/:id",protectRoute,getMessages);
 messageRouter.put("mark/:id",protectRoute,markMessageAsSeen);
+messageRouter.post("/send/:id",protectRoute,sendMessage);
+
 
 export default messageRouter;
 
